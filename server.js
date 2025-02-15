385a152dfb11f19b8786d1e2f35200aabec829a2
Vasyl <vasyl@Administrators-MacBook-Pro.local>
2025-02-15 17:39:57 -0500

    add peer server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import { createRequestHandler } from "@remix-run/express";
 import compression from "compression";
 import express from "express";
 import morgan from "morgan";
+import { ExpressPeerServer } from "peer";
 
 const viteDevServer =
   process.env.NODE_ENV === "production"
@@ -42,10 +43,20 @@ app.use(express.static("build/client", { maxAge: "1h" }));
 
 app.use(morgan("tiny"));
 
-// handle SSR requests
-app.all("*", remixHandler);
-
 const port = process.env.PORT || 3000;
-app.listen(port, () =>
+
+const server = app.listen(port, () =>
   console.log(`Express server listening at http://localhost:${port}`)
 );
+
+const peerServer = ExpressPeerServer(server, {
+  proxied: true,
+  debug: true,
+  path: "/peer",
+  ssl: {},
+});
+
+app.use(peerServer);
+
+// handle SSR requests
+app.all("*", remixHandler);
